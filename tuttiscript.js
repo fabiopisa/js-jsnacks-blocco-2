70c12a71760eec4cabca287134a371c1bc4a5634
fabiopisa <pisanellifabio@gmail.com>
2021-04-13 23:25:16 +0200

    commit piccola correzione

--- tuttiscript.js
+++ tuttiscript.js
@@ -27,7 +27,7 @@ var numero_inserito = parseInt(prompt("digita un numero"))
 if(numero_inserito % 2 === 0){
   console.log("il numero " + numero_inserito + " è pari")
 }else{
-  numero_arrotondato = numero_inserito + 1;
+   var numero_arrotondato = numero_inserito + 1;
   console.log("il numero " + numero_inserito + " è dispari, aggiungo 1, quindi diventa " + numero_arrotondato + " ed è pari")
 }
 /* /JSnack 2 B-2 */
